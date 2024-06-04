0ba9815bb175d4c0941d8daf3deb4035b6a1438e
kartikg1809 <kartikg1809@gmail.com>
2024-06-05 02:07:27 +0530

    Completed sign in page routes and styling

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,10 +1,11 @@
 import User from '../models/userModel.js'
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup=async (req,res,next)=>{
     const {username,email,password}=req.body;
-    const hashPass=bcryptjs.hash(password,10);
+    const hashPass=bcryptjs.hashSync(password,10);
     const newUser=new User({username,email,password:hashPass});
     try {
         await newUser.save();
@@ -13,3 +14,20 @@ export const signup=async (req,res,next)=>{
         next(error);
     }  
 };
+
+export const signin=async (req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, "User not found"));
+            const validPass=bcryptjs.compareSync(password,validUser.password);
+            if(!validPass){
+                return next(errorHandler(401,"Wrong credentials"));
+            }
+            const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET);
+            const { password: pass , ...rest}=validUser._doc;   //cause we dont want to send the password to the user
+            res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target: 'http://localhost:3000',
+        secure:false,
+      }
+    }
+  },
   plugins: [preact()],
 })
